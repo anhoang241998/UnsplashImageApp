12ad0b259a5185ed759f165f186aaddd50c78942
Nguyễn Hoàng Ân <an2419998@gmail.com>
2021-02-13 18:31:05 +0700

    setup

---
+++ app/src/main/java/com/annguyenhoang/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.annguyenhoang.imagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.annguyenhoang.imagesearchapp.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
